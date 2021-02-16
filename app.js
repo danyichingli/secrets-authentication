5e22a1f081a283c63dd178963e19bb5ec0d3b22d
danyichingli <danyichingli@gmail.com>
2021-02-16 17:22:20 -0500

    Level 4: Salting and Hashing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 =  require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -42,11 +43,13 @@ app.post("/login", function(req, res){
     if (err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        if (foundUser.password === password){
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if (result){
           res.render("secrets");
+        } else {
+          console.log(err);
         }
-      }
+      });
     }
   });
 });
@@ -56,17 +59,19 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser =  new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser =  new User({
+      email: req.body.username,
+      password: hash
+    });
 
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
